022a5748f905e7f9e70ab83443cbb9426f7f002b
Hyun Ho Lee <76906638+Hyun-ho-Lee@users.noreply.github.com>
2022-08-02 02:01:18 +0900

    Add files via upload

---
+++ tformer_model.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  2 01:59:43 2022
+
+@author: 이현호
+"""
+
+import torch
+import torch as nn 
+
+
+from seq2seq_model import Encoder
+from seq2seq_model import Decoder 
+
+""" transformer """
+class Transformer(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.encoder = Encoder(self.config)
+        self.decoder = Decoder(self.config)
+    
+    def forward(self, enc_inputs, dec_inputs):
+        # (bs, n_enc_seq, d_hidn), [(bs, n_head, n_enc_seq, n_enc_seq)]
+        enc_outputs, enc_self_attn_probs = self.encoder(enc_inputs)
+        # (bs, n_seq, d_hidn), [(bs, n_head, n_dec_seq, n_dec_seq)], [(bs, n_head, n_dec_seq, n_enc_seq)]
+        dec_outputs, dec_self_attn_probs, dec_enc_attn_probs = self.decoder(dec_inputs, enc_inputs, enc_outputs)
+        # (bs, n_dec_seq, n_dec_vocab), [(bs, n_head, n_enc_seq, n_enc_seq)], [(bs, n_head, n_dec_seq, n_dec_seq)], [(bs, n_head, n_dec_seq, n_enc_seq)]
+        return dec_outputs, enc_self_attn_probs, dec_self_attn_probs, dec_enc_attn_probs
+
